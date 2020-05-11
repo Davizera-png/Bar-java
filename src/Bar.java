4d62c181aba93f51a1efc704cc4bb083c43dd002
Davizera-png <olidavidson34@gmail.com>
2020-05-11 17:45:56 -0300

    Leitura dos dados

--- src/Bar.java
+++ src/Bar.java
@@ -1,8 +1,26 @@
-
+import java.util.Scanner;
+import java.util.Locale;
 public class Bar {
 
 	public static void main(String[] args) {
-	
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+        int cev, esp, ref;
+        char sexo;
+        double tcev, tesp, tref, ing, cons, pagar, couv;
+
+        System.out.println("Sexo: ");
+        sexo = sc.next().charAt(0);
+
+        System.out.println("Quantidade de cervejas: ");
+        cev = sc.nextInt();
+
+        System.out.println("Quantidade de refrigerantes: ");
+        ref = sc.nextInt();
+
+        System.out.println("quantidade de espetinhos");
+        esp = sc.nextInt();
 
 	}
 
